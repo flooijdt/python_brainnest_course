3a9860986a68b1dc3193cd7c9a0df9379d59a8d2
flooijdt <flooijdt@posteo.de>
2023-01-25 16:41:35 -0300

    default commit

---
+++ guess_game.py
@@ -0,0 +1,10 @@
+response = input("Choose a, b or c: ")
+
+if response == "a":
+    print("Correct!")
+elif response == "b":
+    print("Wrong!")
+elif response == "c":
+    print("Wrong!")
+else:
+    print("invalid input.")
