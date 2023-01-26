cef7c401ae889cb1d5535a4bebe8855de560b0b3
flooijdt <flooijdt@posteo.de>
2023-01-26 15:38:05 -0300

    default commit

--- score.py
+++ score.py
@@ -1,21 +1,24 @@
 number = input("Input a number from 0.0 to 1.0: ")
 
-try:
-    not isinstance(number, str)
-except:
-    print("You entered a string!")
+def computegrade(number):
+    try:
+        float(number)
+    except:
+        print("You entered a string!")
 
-number = float(number)
+    number = float(number)
 
-if number >= 0.9:
-    print("A")
-elif number >= 0.8:
-    print("B")
-elif number >= 0.7:
-    print("C")
-elif number >= 0.6:
-    print("D")
-elif number < 0.6:
-    print("F")
-else:
-    print("something went wrong.")
+    if number >= 0.9:
+        return "A"
+    elif number >= 0.8:
+        return "B"
+    elif number >= 0.7:
+        return "C"
+    elif number >= 0.6:
+        return "D"
+    elif number < 0.6:
+        return "F"
+    else:
+        return("something went wrong.")
+
+print(computegrade(number))
