e9cbc8120731b1f0015e4a0fd43aaec6ee32034b
flooijdt <flooijdt@posteo.de>
2023-01-26 15:26:13 -0300

    default commit

--- input.py
+++ input.py
@@ -3,12 +3,13 @@ rate = input("Please, enter rate: ")
 
 hours = float(hours)
 rate = float(rate)
-try:
-    type(hours) != str
-except:
-    print("you wrote a string!")
 
-if hours > 40:
-    print("Pay: " + str(1.5 * hours * rate))
-else:
-    print("Pay: " + str(hours * rate))
+
+def payment(hours, rate):
+    if hours > 40:
+        print("Pay: " + str(1.5 * hours * rate))
+    else:
+        print("Pay: " + str(hours * rate))
+
+
+payment(hours, rate)
