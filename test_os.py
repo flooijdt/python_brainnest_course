73be4fd92198df28104044e51e3eda1d2470bd30
flooijdt <flooijdt@posteo.de>
2023-01-30 10:26:52 -0300

    default commit

--- test_os.py
+++ test_os.py
@@ -3,3 +3,18 @@ import os
 for file in os.listdir('.'):
     if file.startswith('attachment'):
         print(file)
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(file.read())
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to file part
+        part.add_header(
+            "Content-Disposition",
+            f"file; filename= {file}",
+        )
+
+        # Add file to message and convert message to string
+        message.attach(part)
+        text = message.as_string()
+
+
