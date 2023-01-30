29339c7cbe1559efe2cb69a6edb44b67e809da4a
flooijdt <flooijdt@posteo.de>
2023-01-30 09:29:49 -0300

    default commit

--- mailing_script.py
+++ mailing_script.py
@@ -1,4 +1,6 @@
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 port = 465
 smtp_server = "smtp.gmail.com"
@@ -7,15 +9,41 @@ receiver_email = "paratestes868@gmail.com"
 password = input("input the password: ")
 
 
-message = f"""\
-Sender: {sender_email}
-Receiver: {receiver_email}
-Subject: automating e-mail sending
+message = MIMEMultipart("alternative")
+message["Subject"] = "multipart test"
+message["From"] = sender_email
+message["To"] = receiver_email
 
-This is the body of the email."""
+# Create the plain-text and HTML version of your message
+text = """\
+Hi,
+How are you?
+Real Python has many great tutorials:
+www.realpython.com"""
+
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       <a href="http://www.realpython.com">Real Python</a> 
+       has many great tutorials.
+    </p>
+  </body>
+</html>
+"""
+
+# Turn these into plain/html MIMEText objects
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(part1)
+message.attach(part2)
 
 context = ssl.create_default_context()
 # Creates smtp server.
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_email, message.as_string())
