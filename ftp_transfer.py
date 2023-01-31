764ca808848a1f942329dcc7f1c9a0efe65e7ead
flooijdt <flooijdt@posteo.de>
2023-01-31 09:27:16 -0300

    default commit

---
+++ ftp_transfer.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+# Intormation to access the public FTP server used
+# are available at https://dlptest.com/ftp-test/.
+# be sure to visit the site since the password changes
+# from time to time.
+import ftplib
+
+# FTP server information:
+hostname = "ftp.dlptest.com"
+username = "dlpuser@dlptest.com"
+password = "rNrKYTX9g7z3RgJRmxWuGHbeu"
+
+# Create server
+ftp_server = ftplib.FTP(hostname, username, password)
+
+# force UTF-8 encoding
+ftp_server.encoding = "utf-8"
+
+# Enter filename to store with extension
+filename = "attachment.txt"
+
+# Read file in binary mode
+with open(filename, "rb") as file:
+    # Upload the file "STOR filename"
+    ftp_server.storbinary(f"STOR {filename}", file)
+
+# Get list of files
+ftp_server.dir()
+
+# Enter filename to retrieve
+filename = "attachment.txt"
+
+# Write file in binary mode
+with open(filename, "wb") as file:
+    # Download the file "RETR filename"
+    ftp_server.retrbinary(f"RETR {filename}", file.write)
+
+# Display the content of downloaded file
+file = open(filename, "r")
+print('File Content:', file.read())
+
+# Close the Connection
+ftp_server.quit()
