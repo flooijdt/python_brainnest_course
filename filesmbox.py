61caa132d263afdc2b1bbb3fcde5195be2218cf1
flooijdt <flooijdt@posteo.de>
2023-02-01 17:30:10 -0300

    default commit

---
+++ filesmbox.py
@@ -0,0 +1,13 @@
+file = open("mbox.txt")
+text = file.read()
+listatest = text.split()
+lista = []
+counter = 0
+for i in listatest:
+    if i == "From:":
+        lista.append("Sender: ")
+        lista.append(listatest[counter+1])
+    counter += 1
+print(lista)
+            
+        
