27dd327177d3e7c8795a4ba099358ec9ee7a86cb
flooijdt <flooijdt@posteo.de>
2023-02-03 08:47:44 -0300

    default commit

--- primeset.py
+++ primeset.py
@@ -1,10 +1,21 @@
 set1 = [1,2,3,4,5,6,7,8,9]
-set2 = [1,2,3,4,5,6,7,8,9]
+primes = set()
+# Why it says when remove(i): i dont exist?
+# for i in set1:
+#     if i != 1 and i != 2:
+#         for j in range(2,i):
+#             if i % j == 0:
+              # set2.remove(i)
+
+def is_prime(n):
+    prime = True
+    if n != 1:
+        for i in range(2,n):
+            if n % i == 0:
+                prime = False
+    return prime
 
 for i in set1:
-    if i != 1:
-        for j in range(2, i-1):
-            if (i % j) == 0:
-                set2.remove(i-1)
-
-print(set2)
+    if is_prime(i):
+        primes.add(i)
+print(primes)
