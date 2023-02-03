1abe88e5f54e897d2200c8647cba63702bcd6b7f
flooijdt <flooijdt@posteo.de>
2023-02-03 07:43:07 -0300

    default commit

--- anagrams.py
+++ anagrams.py
@@ -2,10 +2,12 @@ lista = ["eat", "tea", "tan", "ate", "nat", "bat"]
 lista2 = []
 lista3 = []
 lista4 = []
+lista5 = []
 for i in lista:
     lista2.append(list(i))
     lista3.append(sorted(list(i)))
+    lista5.append(set(list(i)))
 for i in lista2:
     if lista3.count(sorted(i)) > 1:
         lista4.append("".join(i))
-print(sorted(lista4))
+print(lista5)
