98727c7f72175bfe22ce1539d70749cb436229f9
flooijdt <flooijdt@posteo.de>
2023-02-03 16:34:46 -0300

    default commit

--- dddicter.py
+++ dddicter.py
@@ -1,12 +1,12 @@
 def dicter(dicts):
     for i in dicts:
-        for k, v in i:
+        for k, v in i.items():
             try:
                 if k == "a":
                     return v
             except KeyError:
                 continue
 
-dicts = [{"a" : 1, "b": 2}, {"c" : 1, "b": 2}, {"d" : 1, "b": 2}]
+dicts = [{"a": 1, "b": 2, "f": 6}, {"c": 1, "b": 2}, {"d": 1, "b": 2}]
 
 print(dicter(dicts))
