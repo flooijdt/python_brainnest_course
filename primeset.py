fd16b0391a8936913ab2a9cd3ccd550b14685097
flooijdt <flooijdt@posteo.de>
2023-02-03 07:35:21 -0300

    default commit

--- primeset.py
+++ primeset.py
@@ -1,10 +1,10 @@
-set1 = {1,2,3,4,5,6,7,8,9}
-set2 = {1,2,3,4,5,6,7,8,9}
+set1 = [1,2,3,4,5,6,7,8,9]
+set2 = [1,2,3,4,5,6,7,8,9]
 
 for i in set1:
     if i != 1:
-        for j in range(2, i):
+        for j in range(2, i-1):
             if (i % j) == 0:
-                set2.remove(i)
+                set2.remove(i-1)
 
 print(set2)
