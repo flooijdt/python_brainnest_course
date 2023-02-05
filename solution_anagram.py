32691dbc5fc3aa044acf6fb8a1ad8607da3eb284
flooijdt <flooijdt@posteo.de>
2023-02-05 11:01:47 -0300

    default commit

--- solution_anagram.py
+++ solution_anagram.py
@@ -4,6 +4,7 @@ def anagrams(lst):
         sorted_word = ''.join(sorted(word))
         if sorted_word in d:
             d[sorted_word].append(word)
+            print(d)
         else:
             d[sorted_word] = [word]
     return [set(v) for v in d.values() if len(v) > 1]
