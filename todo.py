fc10c9bc3d3e80decbe10cfa8ff3c58fee95fb52
flooijdt <flooijdt@posteo.de>
2023-02-07 09:24:15 -0300

    default commit

--- todo.py
+++ todo.py
@@ -23,7 +23,29 @@ def delete_todo():
 
 
 display_inbox(lst)
+command = input("Do you want to complete a todo (c)? Delete a \
+    todo (d)? or create a new todo (n)? input 'c', 'd', or 'n':\n")
+if command == "c":
+    n = input("input the desired todo number:\n")
+    todo = lst[n]
+    if todo[0] == 1:
+        print("The todo is already done!\n")
+    elif todo[0] == 0:
+        todo[0] = 1
+        print(f"todo {n} is now done!")
+    else:
+        print("Invalid todo number.")
+elif command == "d":
+    n = input("Enter the soon to be deleted todo number:\n")
+    if todo[0] == 1:
+        print("The todo is already done!\n")
+    elif todo[0] == 0:
+        todo[0] = 1
+        print(f"todo {n} is now done!")
+    else:
+        print("Invalid todo number.")
 
-todo = {"Undone": input("Input your new todo:\n")}
+
+# todo = ["Undone", input("Input your new todo:\n")]
 
 print(todo)
