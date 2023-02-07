d4f4380782bde7d41f060e709de82355c681c700
flooijdt <flooijdt@posteo.de>
2023-02-07 08:54:06 -0300

    default commit

---
+++ todo.py
@@ -0,0 +1,21 @@
+
+lst = []
+line = "-----------------------------"
+
+
+def display_inbox():
+    for todo in lst:
+        print(todo)
+        print(line)
+
+
+def add_todo():
+    pass
+
+
+def complete_todo():
+    pass
+
+
+def delete_todo():
+    pass
