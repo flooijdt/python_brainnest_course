8b3b108c60cf7f8c02a04ce3e2913ccf646f7ec6
flooijdt <flooijdt@posteo.de>
2023-02-07 16:44:49 -0300

    default commit

---
+++ chino.py
@@ -0,0 +1,26 @@
+import ftplib
+
+# Fill Required Information
+HOSTNAME = "ftp.dlptest.com"
+USERNAME = "dlpuser"
+PASSWORD = "rNrKYTX9g7z3RgJRmxWuGHbeu"
+
+# Connect FTP Server
+ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
+
+# force UTF-8 encoding
+ftp_server.encoding = "utf-8"
+
+# Enter File Name with Extension
+filename = "gfg.txt"
+
+# Read file in binary mode
+with open(filename, "rb") as file:
+    # Command for Uploading the file "STOR filename"
+    ftp_server.storbinary(f"STOR {filename}", file)
+
+# Get list of files
+ftp_server.dir()
+
+# Get list of files
+ftp_server.dir()
