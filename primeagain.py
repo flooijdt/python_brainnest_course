34e476541a09e92f6e7c42ac89b181a4a62d7731
flooijdt <flooijdt@posteo.de>
2023-02-08 16:52:13 -0300

    default commit

---
+++ primeagain.py
@@ -0,0 +1,22 @@
+def is_prime(n):
+    prime = True
+    if n != 1:
+        for i in range(2,n):
+            if n % i == 0:
+                prime = False
+    return prime
+
+def prime_num(num):
+    lst = []
+    counter = 0
+    for i in range(100):
+        if counter < num:
+            if is_prime(i):
+                lst.append(i)
+                counter += 1
+        else:
+            continue
+    print(lst)
+
+prime_num(5)
+prime_num(10)
