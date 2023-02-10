eaca22578f5aa056393ea0e1ec8f69adff1e1d00
flooijdt <flooijdt@posteo.de>
2023-02-10 19:02:20 -0300

    default commit

--- regex_project.py
+++ regex_project.py
@@ -5,5 +5,8 @@ import re
 with open('textregex.txt') as f:
     for line in f:
         match = re.findall(r"\w+", line)
-        dictio = {'Order Number': match[1], 'Customer': match[], 'Items': {'Item 1': 2, 'Item 2': 1, 'Item 3': 3}}
-        print(match)
+        items = [i for i in match[4:]]
+        items_dict = {line.index(i): i for i in items}
+        dictio = {'Order Number': match[1], 'Customer': match[2] + ' ' +
+                  match[3], 'Items': items_dict}
+        print(dictio)
