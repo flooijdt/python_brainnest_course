c802e97c4ffe96b7cfad7d437c48184d1b97e2ba
flooijdt <flooijdt@posteo.de>
2023-02-10 18:52:21 -0300

    default commit

--- regex_project.py
+++ regex_project.py
@@ -1,8 +1,9 @@
 import re
 #{'Order Number': 123, 'Customer': 'John Doe', 'Items': {'Item 1': 2, 'Item 2': 1, 'Item 3': 3}}
+# ['Order', '123', 'John', 'Doe', 'Itema', 'Itemb', 'Itemc']
 
 with open('textregex.txt') as f:
     for line in f:
         match = re.findall(r"\w+", line)
-        dictio = {'Order'}
+        dictio = {'Order Number': match[1], 'Customer': match[], 'Items': {'Item 1': 2, 'Item 2': 1, 'Item 3': 3}}
         print(match)
