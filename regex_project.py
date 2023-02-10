23c078f1a1be8aa9c30555d1671a23d1098834c1
flooijdt <flooijdt@posteo.de>
2023-02-10 19:27:28 -0300

    default commit

--- regex_project.py
+++ regex_project.py
@@ -1,18 +1,30 @@
+#!/bin/python3
+# author: Camilo Pimentel
+# date_created: 10/02/2023
+# last_modified: 10/02/2023
+
+# description:
+# Takes a 'textregex.txt' file with customer data as input and
+# outputs a 'regex_output.txt' with organized customer data.
+
 import re
-#{'Order Number': 123, 'Customer': 'John Doe', 'Items': {'Item 1': 2, 'Item 2': 1, 'Item 3': 3}}
-# ['Order', '123', 'John', 'Doe', 'Itema', 'Itemb', 'Itemc']
 
-with open('textregex.txt') as f:
-    for line in f:
-        match = re.findall(r"\w+", line)
-        items = [i for i in match[4:]]
-        items_dict = {items.index(i): i for i in items}
-        dictio = {'Order Number': match[1], 'Customer': match[2] + ' ' +
-                  match[3], 'Items': items_dict}
-        with open('regex_output.txt', 'a') as o:
-            o.write(str(dictio)+'\n')
+try:
+    with open('textregex.txt') as f:
+        for line in f:
+            match = re.findall(r"\w+", line)
+            items = [i for i in match[4:]]
+            items_dict = {items.index(i): i for i in items}
+            dictio = {'Order Number': match[1], 'Customer': match[2] + ' ' +
+                      match[3], 'Items': items_dict}
+            with open('regex_output.txt', 'a') as o:
+                o.write(str(dictio)+'\n')
+except OSError:
+    print("Could not open designated file to read.")
 
-
-with open('regex_output.txt') as f:
-    for line in f:
-        print(f.read())
+try:
+    with open('regex_output.txt') as f:
+        for line in f:
+            print(f.read())
+except OSError:
+    print("Could not write output to file.")
