f30fb50d8b02c4068bc020924d7e5158bd7fb2e7
flooijdt <flooijdt@posteo.de>
2023-02-10 18:50:37 -0300

    default commit

--- regex_project.py
+++ regex_project.py
@@ -1,6 +1,8 @@
 import re
+#{'Order Number': 123, 'Customer': 'John Doe', 'Items': {'Item 1': 2, 'Item 2': 1, 'Item 3': 3}}
 
 with open('textregex.txt') as f:
     for line in f:
         match = re.findall(r"\w+", line)
+        dictio = {'Order'}
         print(match)
