f4b678aaa2916c788848d9ee5927ad1aa25b87d2
flooijdt <flooijdt@posteo.de>
2023-02-10 19:03:58 -0300

    default commit

--- regex_project.py
+++ regex_project.py
@@ -6,7 +6,7 @@ with open('textregex.txt') as f:
     for line in f:
         match = re.findall(r"\w+", line)
         items = [i for i in match[4:]]
-        items_dict = {match.index(i): i for i in items}
+        items_dict = {items.index(i): i for i in items}
         dictio = {'Order Number': match[1], 'Customer': match[2] + ' ' +
                   match[3], 'Items': items_dict}
         print(dictio)
