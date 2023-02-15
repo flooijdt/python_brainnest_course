e416444f167bd119f0a6a5f83d577df969a23ff7
flooijdt <flooijdt@posteo.de>
2023-02-15 16:45:00 -0300

    default commit

--- lambda3.py
+++ lambda3.py
@@ -2,3 +2,7 @@
 # returns a new list of dictionaries sorted by a specified key.
 
 list_o_dicts = [{"a": 1}, {"b": 2}, {"c": 3}, {"d": 4}]
+
+lista = list(filter(lambda x: x == "a" or x == "d", list_o_dicts))
+
+print(lista)
