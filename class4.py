3524287a44e3c8095e48cee573b76f1e64d06c22
flooijdt <flooijdt@posteo.de>
2023-02-15 15:55:39 -0300

    default commit

---
+++ class4.py
@@ -0,0 +1,3 @@
+Define a class BankAccount with a class variable bank_name and instance variables account_holder_name, balance.
+Initialize the class variables in the constructor and add methods like deposit() and withdraw() to perform transactions.
+Create two instances of the BankAccount class and verify that the class variable is shared among all instances.
