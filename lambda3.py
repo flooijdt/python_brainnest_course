20ae4676c5739ac2c3c38a7bc3e9f962f58a7913
flooijdt <flooijdt@posteo.de>
2023-02-15 17:11:43 -0300

    default commit

--- lambda3.py
+++ lambda3.py
@@ -1,6 +1,6 @@
 list_o_dicts = [{"a": 1}, {"b": 2}, {"c": 3}, {"d": 4}]
 
 lista = list(filter(
-    lambda x: [i for i in x if "a" in i or "d" in i], list_o_dicts))
+    lambda x: [i for i in x if [k, v in for k, v in i.items() if v > 2]], list_o_dicts))
 
 print(lista)
