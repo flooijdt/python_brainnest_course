4caaac3366f3160958f4ba13a74715391f4f5cc8
flooijdt <flooijdt@posteo.de>
2023-02-15 16:40:09 -0300

    default commit

--- lambda2.py
+++ lambda2.py
@@ -1,6 +1,6 @@
 # 1. Write a lambda function that takes a list of numbers, and returns
 # a new list containing only the positive numbers from the original list.
 lister = [1,2,3,4,5,-1,-2]
-lista = list(filter(lambda x: x > 0), lister)
+lista = list(filter(lambda x: x > 0, lister))
 
 print(lista)
