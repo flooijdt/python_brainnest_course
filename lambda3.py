53c24a49b87d80872b6cfce4fef7b3384fdeaae4
flooijdt <flooijdt@posteo.de>
2023-02-15 16:42:40 -0300

    default commit

--- lambda3.py
+++ lambda3.py
@@ -1,2 +1,4 @@
 # 2. Write a lambda function that takes a list of dictionaries and
 # returns a new list of dictionaries sorted by a specified key.
+
+list_o_dicts = [{"a": 1}, {"b": 2}, {"c": 3}, {"d": 4}]
